fde93e524a043c24f66e741b7087463cfd8c6cf3
qjwvtd <176156562@qq.com>
2020-09-15 09:39:07 +0800

    Add files via upload

--- src/view/index.js
+++ src/view/index.js
@@ -1,44 +1,5 @@
-import React, { Fragment } from 'react';
-// import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { Router, Route, Switch } from 'react-router-dom';
-import history from '@/common/router';
-//全局顶层模块
-import { PageNoFind, Error } from '@/view/exception';
-import Main from '@/view/main';
-import HomeWapper from '@/view/home';
-import Login from '@/view/login';
-import UpdatePassword from '@/view/updatePassword';
-
-
-//1
-import Module1 from '@/view/module1';
-//2
-import Module2 from '@/view/module2';
-//3
-import Module3 from '@/view/module3';
-//4
-import Module4 from '@/view/module4';
-
-
-export default function View() {
-    return <Fragment>
-        <Router history={history}>
-            <Switch>
-                <Route exact path="/" component={Main} />
-                <Route path="/home">
-                    <HomeWapper>
-                        {/* 首页默认加载成员管理 */}
-                        <Route exact path="/home/module1" component={Module1}></Route>
-                        <Route path="/home/module2" component={Module2}></Route>
-                        <Route path="/home/module3" component={Module3}></Route>
-                        <Route path="/home/module4" component={Module4}></Route>
-                    </HomeWapper>
-                </Route>
-                <Route path="/login" component={Login} />
-                <Route path="/updatePassword" component={UpdatePassword} />
-                <Route path="/error" component={Error} />
-                <Route component={PageNoFind} />
-            </Switch>
-        </Router>
-    </Fragment >;
-}
+import { PageNotFound } from '@/view/exception/404';
+import { Error } from '@/view/exception/error';
+module.exports = {
+    PageNotFound, Error
+};
