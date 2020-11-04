670e534fd74affe201c71c366b74421d06801d71
qjwvtd <176156562@qq.com>
2020-11-04 11:40:27 +0800

    Update index.js

--- src/common/http/index.js
+++ src/common/http/index.js
@@ -48,13 +48,6 @@ axios.interceptors.request.use(config => {
             config.headers.common[author] = 'Bearer ' + token;
         }
     }
-    // 设置请求的cancelToken,以中断未完成的请求
-    config.headers.common['router'] = router.location.pathname;
-    if (config.headers.common.router !== router.location.pathname) {
-        const CancelToken = axios.CancelToken;
-        config.cancelToken = new CancelToken();
-        config.cancelToken.source().cancel();
-    }
     return config;
 }, error => Promise.reject(error));
 
