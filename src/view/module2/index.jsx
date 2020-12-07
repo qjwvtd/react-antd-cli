cbe5e0c927971571c2e92d9a84c5f942546c7e5a
张小军 <zhang.xiaojun@ttwx365.com>
2020-12-07 14:03:01 +0800

    2020-12-07,14:02

--- src/view/module2/index.jsx
+++ src/view/module2/index.jsx
@@ -1,7 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import arrayToLinkList from '@/common/utils/linkList';
 const Module2 = () => {
+    const [linkList, setLinkList] = useState({});
+    useEffect(() => {
+        const arr = [
+            { name: 'Andy', age: 12 },
+            { name: 'Tom', age: 52 },
+            { name: 'Pit', age: 62 },
+            { name: 'Cat', age: 42 },
+            { name: 'Jety', age: 32 },
+            { name: 'Jake', age: 5 }
+        ];
+        const linkListData = arrayToLinkList(arr);
+        setLinkList(linkListData);
+    }, []);
     return <Fragment>
-        <p>模块2</p>
+        <p>数组转化为链表</p>
+        <div className="autowrap">{JSON.stringify(linkList)}</div>
     </Fragment>;
 };
 
