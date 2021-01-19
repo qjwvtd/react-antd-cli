78e5d26be5b30b0f16bdbab92c85cdc9e99855fa
张小军 <zhang.xiaojun@ttwx365.com>
2021-01-19 15:38:35 +0800

    2021-01-19,15;387

--- src/view/home_hook_state/demo1/store/index.js
+++ src/view/home_hook_state/demo1/store/index.js
@@ -2,11 +2,9 @@ import createLiveStore from './../lib';
 import user from './reducer/user';
 import project from './reducer/project';
 import girl from './reducer/girl';
-import { initGirl, initProject } from './action';
 
 const reducerMap = { user, project, girl };
-const actionMap = { initGirl, initProject };
-const liveStore = createLiveStore(reducerMap, actionMap);
+const liveStore = createLiveStore(reducerMap);
 
 export const useStore = liveStore.useStore;
 export const Wapper = liveStore.Wapper;
