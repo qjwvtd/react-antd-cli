48ab95e5d7eaa2bbd8d6b028131819cff94bb4ea
张小军 <zhang.xiaojun@ttwx365.com>
2021-02-07 17:43:06 +0800

    2021-02-07,17;45

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -50,7 +50,7 @@ module.exports = merge(webpackConfig, {
         publicPath: base.dev.publicPath
     },
     cache: true,
-    devtool: 'source-map',
+    devtool: 'eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, base.dev.path), //定位静态服务到index.html
         open: false, //是否自动打开浏览器
